43b723e9aa018ff700a93a18b49738fca07f791c
k20059052 <101188329+k20059052@users.noreply.github.com>
2022-12-08 12:28:59 +0000

    update

--- frequencies.py
+++ frequencies.py
@@ -2,28 +2,7 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
- 
     frequencies = {}
-    if len(items) != 0:
-        
-    
-        
-        frequencies[str(items[0])] = 0
-        
     for item in items:
-        item = str(item)
-        if item in frequencies.keys():
-            frequencies[item] += 1
-            
-        else:
-            frequencies[item] = 1
-    print(frequencies)
-    
-    return frequencies
-items = ['a', 'a', 'b', 'b', 'b', 'c']   
-frequencies(items)
-
-def frequencies(items):
-    frequencies = {}
-    # Your code goes here
+        frequencies[str(item)] = frequencies.get(str(item), 0) + 1
     return frequencies
